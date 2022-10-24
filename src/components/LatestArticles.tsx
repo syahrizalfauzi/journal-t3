7a164529971aa631bf9c1d75797f34ce8516cea0
Syahrizal Fauzi <44159978+syahrizalfauzi@users.noreply.github.com>
2022-10-24 12:43:48 +0700

    react-page implementation WIP, basic components done

--- src/components/LatestArticles.tsx
+++ src/components/LatestArticles.tsx
@@ -1,47 +1,42 @@
 import { CellPlugin } from "@react-page/editor";
 import Link from "next/link";
 import React from "react";
+import LayoutProps from "../types/LayoutProps";
 import { parseDateDay } from "../utils/parseDate";
 
-type LatestArticlesProps = {
-  title: string;
-};
+type LatestArticlesProps = LayoutProps;
 
-const LatestArticles = ({ title }: LatestArticlesProps) => {
+const LatestArticles = ({ children }: LatestArticlesProps) => {
   return (
-    <section className="not-prose py-16">
-      <div className="container">
-        <div className="flex flex-row justify-between">
-          <p className="text-3xl font-bold text-white">{title}</p>
+    <section className="py-16">
+      <div className="flex flex-row items-center">
+        <div className="prose max-w-none flex-1">{children}</div>
+        <div className="not-prose">
           <Link href="/archive">
-            <a className="btn btn-outline border-white text-white">See More</a>
+            <a className="not-prose btn self-end text-white">See More</a>
           </Link>
         </div>
-        <div className="mt-8 flex flex-col gap-4">
-          <div className="flex flex-col gap-1 rounded-xl bg-white p-4 shadow-xl">
-            <p className="text-lg font-medium">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt
-              fugiat consequuntur molestias voluptatibus ipsam ut itaque
-              debitis. Facilis, alias repudiandae dolore asperiores tenetur
-              commodi obcaecati debitis doloremque esse consectetur fuga.
-            </p>
-            <p className="italic">Author 1, Author 2</p>
-            <p className="text-gray-400">
-              Available {parseDateDay(new Date())}
-            </p>
-          </div>
-          <div className="flex flex-col gap-1 rounded-xl bg-white p-4 shadow-xl">
-            <p className="text-lg font-medium">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt
-              fugiat consequuntur molestias voluptatibus ipsam ut itaque
-              debitis. Facilis, alias repudiandae dolore asperiores tenetur
-              commodi obcaecati debitis doloremque esse consectetur fuga.
-            </p>
-            <p className="italic">Author 1, Author 2</p>
-            <p className="text-gray-400">
-              Available {parseDateDay(new Date())}
-            </p>
-          </div>
+      </div>
+      <div className="not-prose mt-8 flex flex-col gap-4">
+        <div className="flex flex-col gap-1 rounded-xl bg-white p-4 shadow-xl">
+          <p className="text-lg font-medium">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt
+            fugiat consequuntur molestias voluptatibus ipsam ut itaque debitis.
+            Facilis, alias repudiandae dolore asperiores tenetur commodi
+            obcaecati debitis doloremque esse consectetur fuga.
+          </p>
+          <p className="italic">Author 1, Author 2</p>
+          <p className="text-gray-400">Available {parseDateDay(new Date())}</p>
+        </div>
+        <div className="flex flex-col gap-1 rounded-xl bg-white p-4 shadow-xl">
+          <p className="text-lg font-medium">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt
+            fugiat consequuntur molestias voluptatibus ipsam ut itaque debitis.
+            Facilis, alias repudiandae dolore asperiores tenetur commodi
+            obcaecati debitis doloremque esse consectetur fuga.
+          </p>
+          <p className="italic">Author 1, Author 2</p>
+          <p className="text-gray-400">Available {parseDateDay(new Date())}</p>
         </div>
       </div>
     </section>
@@ -49,23 +44,10 @@ const LatestArticles = ({ title }: LatestArticlesProps) => {
 };
 
 export const latestArticlesPlugin: CellPlugin<LatestArticlesProps> = {
-  Renderer: ({ data }) => <LatestArticles {...data} />,
+  Renderer: ({ children }) => <LatestArticles>{children}</LatestArticles>,
   id: "latestArticlesPlugin",
   title: "Latest Articles",
-  description: "asasdasd",
   version: 1,
-
-  //   controls: {
-  //     type: "autoform",
-  //     schema: {
-  //       properties: {
-  //         title: {
-  //           type: "string",
-  //         },
-  //       },
-  //       required: ["title"],
-  //     },
-  //   },
 };
 
 export default LatestArticles;
--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { useState } from "react";
 import RootLayout from "../components/layout/RootLayout";
 
@@ -6,27 +7,147 @@ import type { Value } from "@react-page/editor";
 import slate from "@react-page/plugins-slate";
 import spacer from "@react-page/plugins-spacer";
 import divider from "@react-page/plugins-divider";
-import { imagePlugin } from "@react-page/plugins-image";
 import background from "@react-page/plugins-background";
 import { latestArticlesPlugin } from "../components/LatestArticles";
-import LayoutProps from "../types/LayoutProps";
+import { TextField } from "@mui/material";
+import MenuItem from "@mui/material/MenuItem";
+import classNames from "classnames";
+import { FaImage } from "react-icons/fa";
 
 const containerPlugin: CellPlugin = {
-  Renderer: ({ children }) => <div className="container">{children}</div>,
+  Renderer: ({ children }) => (
+    <div className="container my-auto">{children}</div>
+  ),
   id: "containerPlugin",
   title: "Container",
-  description: "asasdasd",
   version: 1,
+};
+const horizontalDividerPlugin: CellPlugin = {
+  Renderer: () => <div className="divider divider-vertical w-full" />,
+  id: "horizontalDividerPlugin",
+  title: "Horizontal Divider",
+  version: 1,
+  icon: <p>---</p>,
+};
+const verticalDividerPlugin: CellPlugin = {
+  Renderer: () => <div className="divider divider-horizontal h-full" />,
+  id: "verticalDividerPlugin",
+  title: "Vertical Divider",
+  version: 1,
+  icon: <p>|</p>,
+};
 
-  //   controls: {
+type ImageState = {
+  src: string;
+  alt?: string | undefined;
+  height: number;
+  width: number;
+  position: "center" | "left" | "right";
+};
+
+const myImagePlugin: CellPlugin<ImageState> = {
+  id: "myImagePlugin",
+  title: "Image",
+  icon: <FaImage />,
+  version: 1,
+  Renderer: ({ data }) => (
+    <>
+      <img
+        src={data.src}
+        alt={data.alt}
+        style={{
+          height: data.height,
+          width: data.width,
+        }}
+        className={classNames({
+          "mx-auto": data.position === "center",
+          "ml-auto": data.position === "right",
+          "mr-auto": data.position === "left",
+        })}
+      />
+    </>
+  ),
+  createInitialData: () => ({
+    height: 256,
+    width: 256,
+    position: "center",
+    src: "http://localhost:3000/sample.jpeg",
+    alt: "Sample image",
+  }),
+  controls: {
+    type: "custom",
+    Component: ({ data, onChange }) => {
+      return (
+        <div className="grid grid-cols-2 gap-4">
+          <TextField
+            type="text"
+            value={data.src}
+            label="Image link"
+            placeholder="asdasd"
+            fullWidth
+            onChange={(e) => onChange({ src: e.target.value })}
+          />
+          <TextField
+            type="text"
+            value={data.alt}
+            label="Image title"
+            fullWidth
+            onChange={(e) => onChange({ alt: e.target.value })}
+          />
+          <TextField
+            type="number"
+            value={data.height}
+            label="Height (px)"
+            fullWidth
+            onChange={(e) => onChange({ height: Number(e.target.value) })}
+          />
+          <TextField
+            type="number"
+            value={data.width}
+            label="Width (px)"
+            fullWidth
+            onChange={(e) => onChange({ width: Number(e.target.value) })}
+          />
+          <TextField
+            select
+            className="col-span-2"
+            label="Position"
+            value={data.position}
+            onChange={(e) =>
+              onChange({
+                position: e.target.value as "center" | "left" | "right",
+              })
+            }
+          >
+            <MenuItem value="center">Center</MenuItem>
+            <MenuItem value="left">Left</MenuItem>
+            <MenuItem value="right">Right</MenuItem>
+          </TextField>
+        </div>
+      );
+    },
+  },
   //     type: "autoform",
   //     schema: {
   //       properties: {
-  //         title: {
+  //         src: {
   //           type: "string",
+  //           default: "http://localhost:3000/sample.jpeg",
+  //           title: "Image link",
+  //         },
+  //         alt: { type: "string", default: "Sample image", title: "Image title" },
+  //         height: { type: "integer", default: 256, title: "Height (px)" },
+  //         width: { type: "integer", default: 256, title: "Width (px)" },
+  //         isRight: {
+  //           type: "boolean",
+  //           uniforms: {
+  //             showIf: (data) => !data.isCenter,
+  //           },
+  //         },
+  //         isCenter: {
+  //           type: "boolean",
   //         },
   //       },
-  //       required: ["title"],
   //     },
   //   },
 };
@@ -34,17 +155,16 @@ const containerPlugin: CellPlugin = {
 const cellPlugins: CellPluginList = [
   slate(),
   spacer,
-  divider,
-  imagePlugin({}),
   background({}),
+  myImagePlugin,
+  horizontalDividerPlugin,
+  verticalDividerPlugin,
   latestArticlesPlugin,
   containerPlugin,
 ];
-const defaultValue =
-  '{"id":"7bepqn","version":1,"rows":[{"id":"jd7h2q","cells":[{"id":"wgdywf","size":12,"plugin":{"id":"latestArticlesPlugin","version":1},"dataI18n":{"default":{}},"isDraftI18n":{"default":false},"rows":[],"inline":null}]},{"id":"swbc4v","cells":[{"id":"f77c93","size":12,"plugin":{"id":"ory/editor/core/layout/background","version":1},"dataI18n":{"default":{"modeFlag":3}},"rows":[{"id":"o3bpm1","cells":[{"id":"lrucsd","size":12,"plugin":{"id":"ory/editor/core/content/slate","version":1},"dataI18n":{"default":{"slate":[{"type":"PARAGRAPH/PARAGRAPH","children":[{"text":"aasdasdaasd"}]}]}},"rows":[],"inline":null}]}],"inline":null}]}]}';
 
 const EditorPage = () => {
-  const [value, setValue] = useState<Value | null>(JSON.parse(defaultValue));
+  const [value, setValue] = useState<Value | null>(null);
 
   const handleChange = (newValue: Value) => {
     console.log("newValue", JSON.stringify(newValue));
