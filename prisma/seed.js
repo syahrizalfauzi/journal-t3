c0ab02205d34feb20d3efd5aa850a7b9c0af4212
Syahrizal Fauzi <44159978+syahrizalfauzi@users.noreply.github.com>
2022-12-06 07:50:42 +0700

    Seed update for default about & home page

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,7 +2,7 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 async function main() {
-  const settings = await prisma.settings.upsert({
+  const settingsSeed = prisma.settings.upsert({
     where: { id: "settings" },
     update: {},
     create: {
@@ -11,7 +11,29 @@ async function main() {
       reviewersCount: 2,
     },
   });
-  console.log({ settings });
+
+  const aboutPageSeed = prisma.page.upsert({
+    where: {url : "about"},
+    update: {},
+    create: {
+      url: "about",
+      name: "About Page",
+      data: "null",
+    }
+  })
+
+  const homePageSeed = prisma.page.upsert({
+    where: {url : ""},
+    update: {},
+    create: {
+      url: "",
+      name: "Home Page",
+      data: "null",
+    }
+  })
+
+  await Promise.all([settingsSeed, aboutPageSeed, homePageSeed]);
+
 }
 main()
   .then(async () => {
